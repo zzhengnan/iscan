169a9d8d220c1c17c30f255762b273aaed132bf0
Zhengnan Zhao <zzhao212@gmail.com>
2020-03-23 21:57:23 -0400

    Add bare-bones setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="importscanner",
+    version="0.0.1",
+    author="Zhengnan Zhao",
+    description="importscanner helps identify your project's direct dependencies",
+    url="https://github.com/ZhengnanZhao/importscanner",
+    packages=setuptools.find_packages()
+)
