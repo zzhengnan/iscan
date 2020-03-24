1acaae857d4cd4f378938e277f2d65214c183345
Zhengnan Zhao <zzhao212@gmail.com>
2020-03-24 01:51:18 -0400

    Add docstrings and type hints

--- importscanner/scan.py
+++ importscanner/scan.py
@@ -1,9 +1,25 @@
+"""This module provides functionality to scan a repo and extract the names
+of packages imported in said repo."""
+
+
 import os
 import re
 import sys
 
 
-def remove_strings_and_comments(file_content):
+def remove_strings_and_comments(file_content: str) -> str:
+    """Remove strings and comments from a file.
+
+    All forms of single- and multi-line strings enclosed in single (e.g., 'foo' and "foo")
+    and triple quotes will be removed. Likewise, all forms of single-, multi-line,
+    and inline comments will be removed.
+
+    Args:
+        file_content: Original string representation of a file.
+
+    Returns:
+        Content of the original file stripped of strings and comments.
+    """
     triple_quotes = '["\']{3}'
     triple_quotes = triple_quotes + '.*?' + triple_quotes
     triple_quotes = re.compile(triple_quotes, re.DOTALL)  # DOTALL allows matching of multi-line strings
@@ -19,7 +35,22 @@ def remove_strings_and_comments(file_content):
     return file_content
 
 
-def extract_package_name(line):
+def extract_package_name(line: str) -> str:
+    """Extract name of the imported package from a line.
+
+    Work with both forms of import statements.
+    >>> import pandas as pd
+    >>> from pandas import DataFrame
+
+    Known issue: Does not work when importing multiple packages on the same line.
+    >>> import pandas as pd, numpy as np
+
+    Args:
+        line: A single line from a source file.
+
+    Returns:
+        Name of the imported package if applicable, and None otherwise.
+    """
     # Spaces and comments are ignored with the VERBOSE flag
     pattern_1 = re.compile(
         r"""^\s*        # 0 or more spaces. Need the ^ to distinguish from the second pattern
@@ -40,10 +71,21 @@ def extract_package_name(line):
     match_2 = re.search(pattern_2, line)
     if match_2:
         return match_2.groups()[0]
+
     return None
 
 
-def collect_file_paths(path_to_repo):
+def collect_file_paths(path_to_repo: str) -> list:
+    """Collect all python files in a repo.
+
+    Python files refer specifically to files with a .py extension.
+
+    Args:
+        path_to_repo: Path to the repo of interest.
+
+    Returns:
+        List of python files in the repo.
+    """
     fpaths = []
     for root_dir, _, fnames in os.walk(top=path_to_repo):
         for fname in fnames:
@@ -53,7 +95,15 @@ def collect_file_paths(path_to_repo):
     return fpaths
 
 
-def scan_file(file_content):
+def scan_file(file_content: str) -> list:
+    """Extract names of unique packages imported in a file.
+
+    Args:
+        file_content: String representation of a file.
+
+    Returns:
+        List of unique packages imported in the file.
+    """
     imported_pkgs = []
     file_content = remove_strings_and_comments(file_content)
     lines = file_content.split('\n')
@@ -64,7 +114,15 @@ def scan_file(file_content):
     return sorted(set(imported_pkgs))
 
 
-def scan_repo(path_to_repo):
+def scan_repo(path_to_repo: str) -> list:
+    """Extract names of unique packages imported in a repo.
+
+    Args:
+        path_to_repo: Path to the repo of interest.
+
+    Returns:
+        List of unique packages imported in the repo.
+    """
     imported_pkgs = []
     fpaths_to_scan = collect_file_paths(path_to_repo)
     for fpath in fpaths_to_scan:
