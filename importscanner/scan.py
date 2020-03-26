6f20aa19a6399799ff5b0a7a8fcd4de7a6cd5132
Zhengnan Zhao <zzhao212@gmail.com>
2020-03-25 21:24:32 -0400

    Introduce abstract syntax trees

--- importscanner/scan.py
+++ importscanner/scan.py
@@ -4,9 +4,28 @@ and regular strings, all other imported packages will be extracted.
 """
 
 
+import ast
 import os
 import re
 import sys
+
+
+class ImportScanner(ast.NodeVisitor):
+    def __init__(self):
+        self.imports = []
+
+    def visit_Import(self, node):
+        imports = [alias.name for alias in node.names]
+        self.imports.extend(imports)
+        self.generic_visit(node)
+
+    def visit_FromImport(self, node):
+        imports = [alias.name for alias in node.names]
+        self.imports.extend(imports)
+        self.generic_visit(node)
+
+    def report(self):
+        return self.imports
 
 
 def collect_file_paths(path_to_repo: str) -> list:
@@ -38,19 +57,20 @@ def scan_repo(path_to_repo: str) -> list:
     Returns:
         List of unique packages imported in the repo.
     """
-    imported_pkgs = []
     fpaths_to_scan = collect_file_paths(path_to_repo)
+    scanner = ImportScanner()
     for fpath in fpaths_to_scan:
         with open(fpath, 'r') as f:
-            file_content = f.read()
-        imported_pkgs.extend(scan_file(file_content))
-    return sorted(set(imported_pkgs))
+            file_ast = ast.parse(f.read())
+        scanner.visit(file_ast)
+    unique_imports = sorted(set(scanner.report()))
+    return unique_imports
 
 
 def main():
     path_to_repo = sys.argv[1]
-    imported_pkgs = scan_repo(path_to_repo)
-    print('\n'.join(imported_pkgs))
+    unique_imports = scan_repo(path_to_repo)
+    print('\n'.join(unique_imports))
 
 
 if __name__ == '__main__':
