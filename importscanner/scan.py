aea0c8de7b2e635c0a4c5ab11d2e4c9bb3f5d002
Zhengnan Zhao <zzhao212@gmail.com>
2020-05-07 13:52:41 -0400

    Ditch regex for ast

--- importscanner/scan.py
+++ importscanner/scan.py
@@ -1,5 +1,5 @@
-"""This module provides functionality to scan a repo and
-extract the names of packages imported in said repo.
+"""This module provides functionality to scan a repo and aggregate
+the names of packages imported across all python files.
 """
 
 
@@ -10,90 +10,107 @@ import sys
 
 
 class ImportScanner(ast.NodeVisitor):
-    """Scanner to look for import statements.
+    """Scanner to look for import statements."""
 
-    Based on Matt Layman's work at
-    https://www.mattlayman.com/blog/2018/decipher-python-ast/
-    """
     def __init__(self):
         self.imports = []
 
     def visit_Import(self, node):
-        imports = [alias.name for alias in node.names]
-        self.imports.extend(imports)
+        """Extract imports of the form `import foo`.
+
+        >>> import_statement = 'import os.path.join as jn, datetime.datetime as dt'
+        >>> ast.dump(ast.parse(import_statement))
+        "Module(body=[
+            Import(
+                names=[alias(name='os.path.join', asname='jn'),
+                       alias(name='datetime.datetime', asname='dt')]
+            )
+        ])"
+        """
+        for alias in node.names:
+            self.imports.append(alias.name)
         self.generic_visit(node)
 
-    def visit_FromImport(self, node):
-        imports = [alias.name for alias in node.names]
-        self.imports.extend(imports)
+    def visit_ImportFrom(self, node):
+        """Extract imports of the form `from foo import bar`.
+
+        Relative imports such as `from ..utils import foo` will be ignored.
+
+        >>> import_statement = 'from os.path import join as jn, split'
+        >>> ast.dump(ast.parse(import_statement))
+        "Module(body=[
+            ImportFrom(
+                module='os.path',
+                names=[alias(name='join', asname='jn'),
+                       alias(name='split', asname=None)],
+                level=0
+            )
+        ])"
+        """
+        # Ignore relative imports, for which node.level > 0
+        # E.g., `from ..utils import foo` has a node.level of 2
+        if node.level == 0:
+            self.imports.append(node.module)
         self.generic_visit(node)
 
-    def report(self):
+    def get_imports(self):
         return self.imports
 
 
-def collect_file_paths(path_to_repo: str) -> list:
-    """Collect all python files in a repo.
-
-    Python files refer specifically to files with a .py extension.
+def get_base_name(full_name: str) -> str:
+    """Extract the base name of a package/module.
 
     Args:
-        path_to_repo: Path to the repo of interest.
+        full_name: Full name of the package/module of interest. E.g., pandas.testing
 
     Returns:
-        List of python files in the repo.
+        base_name: Base name of the provided package/module. E.g., pandas
     """
-    fpaths = []
-    for root_dir, _, fnames in os.walk(top=path_to_repo):
-        for fname in fnames:
-            if fname.endswith('.py'):
-                full_path = os.path.join(root_dir, fname)
-                fpaths.append(full_path)
-    return fpaths
+    pattern = r'(\w+).*'
+    base_name = re.search(pattern, full_name).groups()[0]
+    return base_name
 
 
-def extract_unique_packages(packages: list) -> list:
-    """Extract unique package names.
-
-    Each element in the returned list is the base name of a package.
-    E.g., if pandas.testing is provided as an input, pandas will get returned.
-
-    Args:
-        packages: A list of packages that may contain duplicates.
-
-    Returns:
-        List of unique package names.
-    """
-    unique_packages = []
-    for package in packages:
-        base_name = re.search(r'(\w+).*', package).groups()[0]
-        unique_packages.append(base_name)
-    return sorted(set(unique_packages))
+def convert_source_to_tree(fpath: str) -> ast.Module:
+    if fpath.endswith('.py'):
+        with open(fpath, 'r') as f:
+            tree = ast.parse(f.read())
+        return tree
 
 
 def scan_repo(path_to_repo: str) -> list:
     """Extract names of unique packages imported in a repo.
 
     Args:
-        path_to_repo: Path to the repo of interest.
+        path_to_repo: Path to the repo of interest
 
     Returns:
-        List of unique packages imported in the repo.
+        List of unique packages imported in the repo
     """
-    fpaths_to_scan = collect_file_paths(path_to_repo)
-    scanner = ImportScanner()
-    for fpath in fpaths_to_scan:
-        with open(fpath, 'r') as f:
-            file_ast = ast.parse(f.read())
-        scanner.visit(file_ast)
-    unique_packages = extract_unique_packages(scanner.report())
-    return unique_packages
+    all_imports = []
+    for root_dir, subdirs, fnames in os.walk(top=path_to_repo):
+        # TODO: Get name of package under investigation and add to this
+        local_modules = subdirs + [fname.replace('.py', '') for fname in fnames]
+
+        for fname in fnames:
+            # Convert source code into tree
+            fpath = os.path.join(root_dir, fname)
+            tree = convert_source_to_tree(fpath)
+
+            # Extract imports for current file
+            scanner = ImportScanner()
+            scanner.visit(tree)
+            current_imports = map(get_base_name, scanner.get_imports())
+            current_imports = [imp for imp in current_imports if imp not in local_modules]
+            all_imports.extend(current_imports)
+
+    return sorted(set(all_imports))
 
 
 def main():
     path_to_repo = sys.argv[1]
-    unique_packages = scan_repo(path_to_repo)
-    print('\n'.join(unique_packages))
+    unique_imports = scan_repo(path_to_repo)
+    print('\n'.join(unique_imports))
 
 
 if __name__ == '__main__':
