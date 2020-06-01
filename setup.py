d7fb9e5354835e7a0e3abecff23084935d463ea8
Zhengnan Zhao <zzhao212@gmail.com>
2020-06-01 00:13:31 -0400

    Bump version and add classifiers

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ import setuptools
 
 
 setuptools.setup(
-    name="iscan",
-    version="0.1.0",
-    author="Zhengnan Zhao",
-    description="iscan aggregates your project's direct dependencies",
-    url="https://github.com/ZhengnanZhao/iscan",
+    name='iscan',
+    version='0.2.0',
+    author='Zhengnan Zhao',
+    description='iscan helps you identify your project\'s direct dependencies',
+    url='https://github.com/ZhengnanZhao/iscan',
     packages=setuptools.find_packages(),
     extras_require={
-        "dev": ["pytest", "setuptools", "twine", "wheel"]
+        'dev': ['pytest', 'setuptools', 'twine', 'wheel']
     },
-    entry_points={'console_scripts': ['iscan=iscan.scan:main']}
+    entry_points={'console_scripts': ['iscan=iscan.scan:main']},
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5'
 )
