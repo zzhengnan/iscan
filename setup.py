6e1930e9ad5db22d633c183ee078c82aa2a8fd57
Zhengnan Zhao <zzhao212@gmail.com>
2020-06-01 00:22:36 -0400

    Fix license

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.5'
