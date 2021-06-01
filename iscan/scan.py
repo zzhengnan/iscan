d60b2abdaf0861d221766e327e348c47d2abf79d
Zhengnan Zhao <zzhengnan@protonmail.com>
2021-05-31 23:26:39 -0400

    Refactor into more granular functions

--- iscan/scan.py
+++ iscan/scan.py
@@ -1,17 +1,15 @@
-"""This module provides functionality to scan a directory and aggregate
-the names of packages imported across all python files in said directory.
+"""Utilities to scan all python files in a directory and
+aggregate the names of all the imported packages
 """
 import argparse
 import ast
-from os import walk
-from os.path import abspath, join
+import os
 
-from iscan.std_lib import filter_out_std_lib
+from iscan.std_lib import separate_third_party_from_std_lib
 
 
 class ImportScanner(ast.NodeVisitor):
     """Scanner to look for imported packages."""
-
     def __init__(self):
         self.imports = []
 
@@ -21,10 +19,8 @@ class ImportScanner(ast.NodeVisitor):
         >>> import_statement = 'import os.path.join as jn, datetime.datetime as dt'
         >>> ast.dump(ast.parse(import_statement))
         "Module(body=[
-            Import(
-                names=[alias(name='os.path.join', asname='jn'),
-                       alias(name='datetime.datetime', asname='dt')]
-            )
+            Import(names=[alias(name='os.path.join', asname='jn'),
+                          alias(name='datetime.datetime', asname='dt')])
         ])"
         """
         for alias in node.names:
@@ -39,12 +35,10 @@ class ImportScanner(ast.NodeVisitor):
         >>> import_statement = 'from os.path import join as jn, split'
         >>> ast.dump(ast.parse(import_statement))
         "Module(body=[
-            ImportFrom(
-                module='os.path',
-                names=[alias(name='join', asname='jn'),
-                       alias(name='split', asname=None)],
-                level=0
-            )
+            ImportFrom(module='os.path',
+                       names=[alias(name='join', asname='jn'),
+                              alias(name='split', asname=None)],
+                       level=0)
         ])"
         """
         # Ignore relative imports, for which node.level > 0
@@ -55,18 +49,6 @@ class ImportScanner(ast.NodeVisitor):
 
     def get_imports(self):
         return sorted(self.imports)
-
-
-def get_base_name(full_name: str) -> str:
-    """Extract the base name of a package.
-
-    Args:
-        full_name: Full name of the package of interest, e.g., pandas.testing
-
-    Returns:
-        Base name of the provided package, e.g., pandas
-    """
-    return full_name.split('.')[0]
 
 
 def convert_source_to_tree(fpath: str) -> ast.Module:
@@ -84,19 +66,19 @@ def convert_source_to_tree(fpath: str) -> ast.Module:
 
 
 def scan_directory(dir_to_scan: str, dir_to_exclude: str) -> list:
-    """Extract names of unique packages imported across all python files in a directory.
+    """Extract packages imported across all python files in a directory.
 
     Args:
         dir_to_scan: Path to the directory of interest
 
     Returns:
-        List of unique packages imported
+        List of packages imported; might contain duplicates
     """
     all_imports = []
-    for root_dir, _, fnames in walk(top=dir_to_scan):
+    for root_dir, _, fnames in os.walk(top=dir_to_scan):
         # Skip excluded directory
         if dir_to_exclude is not None:
-            if abspath(dir_to_exclude) in abspath(root_dir):
+            if os.path.abspath(dir_to_exclude) in os.path.abspath(root_dir):
                 continue
 
         for fname in fnames:
@@ -105,7 +87,7 @@ def scan_directory(dir_to_scan: str, dir_to_exclude: str) -> list:
                 continue
 
             # Convert source code into tree
-            fpath = join(root_dir, fname)
+            fpath = os.path.join(root_dir, fname)
             tree = convert_source_to_tree(fpath)
 
             # Extract imports for current file
@@ -113,7 +95,47 @@ def scan_directory(dir_to_scan: str, dir_to_exclude: str) -> list:
             scanner.visit(tree)
             all_imports.extend(scanner.get_imports())
 
-    return sorted(set(map(get_base_name, all_imports)))
+    return all_imports
+
+
+def get_base_name(full_name: str) -> str:
+    """Extract the base name of a package.
+
+    Args:
+        full_name: Full name of the package of interest, e.g., pandas.testing
+
+    Returns:
+        Base name of the provided package, e.g., pandas
+    """
+    return full_name.split('.')[0]
+
+
+def get_unique_baes_packages(packages: list) -> list:
+    """Remove duplicates and extract the base package names.
+
+    Args:
+        packages: List of package names that might contain duplicates
+
+    Returns:
+        List of unique base package names
+    """
+    return sorted(set(map(get_base_name, packages)))
+
+
+def show_result(third_party: list, std_lib: list, ignore_std_lib: bool) -> None:
+    """Print the result of running iscan.
+
+    Args:
+        third_party: List of third-party packages
+        std_lib: List of standard library modules
+        ignore_std_lib: Whether to omit standard library modules in the output
+    """
+    print('\nThird-party packages:\n  - ', end='')
+    print('\n  - '.join(third_party))
+
+    if std_lib and not ignore_std_lib:
+        print('\nModules from the standard library:\n  - ', end='')
+        print('\n  - '.join(std_lib))
 
 
 def cli() -> argparse.Namespace:
@@ -145,17 +167,14 @@ def cli() -> argparse.Namespace:
 
 def main():
     args = cli()
-    dir_to_scan, dir_to_exclude = args.DIR_TO_SCAN, args.DIR_TO_EXCLUDE
 
-    unique_imports = scan_directory(dir_to_scan, dir_to_exclude)
-    unique_imports = filter_out_std_lib(unique_imports)
-    end = ', EXCLUDING those in {}\n'.format(dir_to_exclude) if dir_to_exclude else '\n'
+    all_imports = scan_directory(args.DIR_TO_SCAN, args.DIR_TO_EXCLUDE)
+    unique_imports = get_unique_baes_packages(all_imports)
+    third_party, std_lib = separate_third_party_from_std_lib(unique_imports)
+
     print(
-        'Third-party packages imported across all python files in {}'.format(dir_to_scan),
-        end=end
+        f'Packages imported across all python files in directory "{args.DIR_TO_SCAN}"',
+        end=f', excluding "{args.DIR_TO_EXCLUDE}"\n' if args.DIR_TO_EXCLUDE else '\n'
     )
-    print(unique_imports)
 
-
-if __name__ == '__main__':
-    main()
+    show_result(third_party, std_lib, args.IGNORE_STD_LIB)
